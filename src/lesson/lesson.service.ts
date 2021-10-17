d287f79427d5fd7df1701e5ca64a577dcc8cdacb
Maxwel <maxwelsant96@icloud.com>
2021-10-17 12:08:14 -0300

    get Lessons

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -15,6 +15,10 @@ export class LessonService {
     return this.lessonRepository.findOne({ id });
   }
 
+  async getLessons(): Promise<Lesson[]> {
+    return this.lessonRepository.find();
+  }
+
   async createLesson(createLesson: CreateLessonInput): Promise<Lesson> {
     const { name, startDate, endDate } = createLesson;
     const lesson = this.lessonRepository.create({
